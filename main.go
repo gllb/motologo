368b5a8b59afe35002ee2b87fd50e73ee8302a57
Guillaume Le Blanc <gleblanc@fdn.fr>
2020-11-24 11:51:25 +0100

    Follow golang standard project layout

--- main.go
+++ main.go
@@ -4,12 +4,11 @@ import (
 	"os"
 	"fmt"
 	"errors"
-	"io"
-	"strings"
 	"bytes"
 	"golang.org/x/image/bmp"
 	"encoding/binary"
-	"github.com/gllb/motologo/motorun"
+	"github.com/gllb/motologo/pkg/motologo"
+	"github.com/gllb/motologo/pkg/motorun"
 )
 
 var MOTOLOGO_FILE_LIST = [...]string {"logo_boot", "logo_battery", "logo_unlocked",
@@ -97,58 +96,58 @@ func EncodeMotologo(w io.Writer, m Motologo) error {
 }
 
 // DecodeMotologoFile return a motologo structure according to the content of f
-func DecodeMotologoFile(f *os.File) (Motologo, error) {
-	var motologo Motologo
+// func DecodeMotologoFile(f *os.File) (Motologo, error) {
+// 	var motologo Motologo
 
-	err := binary.Read(f, binary.LittleEndian, &motologo.Header)
-	check(err)
+// 	err := binary.Read(f, binary.LittleEndian, &motologo.Header)
+// 	check(err)
 
-	if string(motologo.Header.Signature[:8]) != "MotoLogo" {
+// 	if string(motologo.Header.Signature[:8]) != "MotoLogo" {
 
-		return Motologo{}, errors.New("motologo: invalid format")
-	}
+// 		return Motologo{}, errors.New("motologo: invalid format")
+// 	}
 
-	motorunMetaCount := (motologo.Header.ItemCount - 0xd)/0x20
-	motologo.MotorunMetaSet = make([]MotorunMeta, motorunMetaCount)
-	motologo.MotorunSet = make([]bytes.Buffer, motorunMetaCount)
+// 	motorunMetaCount := (motologo.Header.ItemCount - 0xd)/0x20
+// 	motologo.MotorunMetaSet = make([]MotorunMeta, motorunMetaCount)
+// 	motologo.MotorunSet = make([]bytes.Buffer, motorunMetaCount)
 
-	err = binary.Read(f, binary.LittleEndian, &motologo.MotorunMetaSet)
-	check(err)
+// 	err = binary.Read(f, binary.LittleEndian, &motologo.MotorunMetaSet)
+// 	check(err)
 
-	for i, motorunMeta := range motologo.MotorunMetaSet {
-		_, err := f.Seek(int64(motorunMeta.Offset), 0)
-		check(err)
+// 	for i, motorunMeta := range motologo.MotorunMetaSet {
+// 		_, err := f.Seek(int64(motorunMeta.Offset), 0)
+// 		check(err)
 
-		img, err := motorun.Decode(f)
-		motorun.Encode(&motologo.MotorunSet[i], img)
-	}
+// 		img, err := motorun.Decode(f)
+// 		motorun.Encode(&motologo.MotorunSet[i], img)
+// 	}
 
-	return motologo, nil
-}
+// 	return motologo, nil
+// }
 
 // Extract create BMP file from Motologo file archive in destDir.
-func Extract(m Motologo, destDir string) error {
-	if err := os.MkdirAll(destDir, 0777); err != nil {
-		return err
-	}
-	for i, motorunBuffer := range m.MotorunSet {
-		name := string(m.MotorunMetaSet[i].Name[:])
-		fmt.Println(name, m.MotorunSet[i].Len())
-		file, err := os.Create(destDir + strings.Trim(name, "\x00") + ".bmp")
-		if err != nil {
-			return err
-		}
+// func Extract(m Motologo, destDir string) error {
+// 	if err := os.MkdirAll(destDir, 0777); err != nil {
+// 		return err
+// 	}
+// 	for i, motorunBuffer := range m.MotorunSet {
+// 		name := string(m.MotorunMetaSet[i].Name[:])
+// 		fmt.Println(name, m.MotorunSet[i].Len())
+// 		file, err := os.Create(destDir + strings.Trim(name, "\x00") + ".bmp")
+// 		if err != nil {
+// 			return err
+// 		}
 
-		img, err := motorun.Decode(&motorunBuffer)
-		if err != nil {
-			return err
-		}
-		if err := bmp.Encode(file, img); err != nil {
-			return err
-		}
-	}
-	return nil
-}
+// 		img, err := motorun.Decode(&motorunBuffer)
+// 		if err != nil {
+// 			return err
+// 		}
+// 		if err := bmp.Encode(file, img); err != nil {
+// 			return err
+// 		}
+// 	}
+// 	return nil
+// }
 
 // Create a motologo structure from the content of srcDir
 func Create(srcDir string) (Motologo, error) {
